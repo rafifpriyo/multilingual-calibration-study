bc226f19f035e840e2d396def3326ab61ed770d2
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2026-04-17 11:03:29 +0700

    Update base script

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+pixi run python -m multilingual_evaluation_globalmmlulite_multiplechoice --model_id Qwen/Qwen3-8B --quantization_technique Unquantized --lang Unquantized --bit 32 2>&1 | tee 'log-globalmmlulite-qwen-Unquantized'
+pixi run python -m multilingual_evaluation_mmluproxlite_generateuntil --model_id Qwen/Qwen3-8B --quantization_technique Unquantized --lang Unquantized --bit 32 2>&1 | tee 'log-mmluproxlite-qwen-Unquantized'
+
 languages=("English" "Indonesian" "Tamil" "Swahili" "Chinese")
-bits=(8 4 3 2)
+bits=(4 3 2)
 quantization_techniques=("tacq" "slimllm" "gptq")
 
 for quant in "${quantization_techniques[@]}"; do
   for bit in "${bits[@]}" ; do
     for lang in "${languages[@]}" ; do
-        pixi run python -m multilingual_evaluation_floresplus_perplexity --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-floresplus-qwen-'$quant$lang$bit
         pixi run python -m multilingual_evaluation_include_multiplechoice --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-include-qwen-'$quant$lang$bit
+        pixi run python -m multilingual_evaluation_floresplus_perplexity --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-floresplus-qwen-'$quant$lang$bit
+        pixi run python -m multilingual_evaluation_mmluproxlite_generateuntil --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-mmluproxlite-qwen-'$quant$lang$bit
         pixi run python -m multilingual_evaluation_globalmmlulite_multiplechoice --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-globalmmlulite-qwen-'$quant$lang$bit
         pixi run python -m multilingual_evaluation_belebele_multiplechoice --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-belebele-qwen-'$quant$lang$bit
         pixi run python -m multilingual_evaluation_multiblimp_multiplechoice --model_id Qwen/Qwen3-8B --quantization_technique "$quant" --lang "$lang" --bit "$bit" --nsamples 128 2>&1 | tee 'log-multiblimp-qwen-'$quant$lang$bit
