2b564e20419f2db0b067a28aa1afcb3cabe4ff10
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2026-07-10 20:55:49 +0700

    Try: edited lm eval for flips metric

--- setting_perplexity_run.py
+++ setting_perplexity_run.py
@@ -2,11 +2,25 @@ import lm_eval
 import evaluate
 import os
 import shutil
+# LM Eval Harness edit
+# Max length error, extended 1 token
 util_source_path = f"./edited_vllm_causallms.py"
 util_update_path = f"{os.path.join(os.path.dirname(lm_eval.__file__), f'models/vllm_causallms.py')}"
 
 shutil.copyfile(util_source_path, util_update_path)
 
+# True False answers for flips metric
+util_source_path = f"./edited_lm_eval/metrics.py"
+util_update_path = f"{os.path.join(os.path.dirname(lm_eval.__file__), f'api/model.py')}"
+
+shutil.copyfile(util_source_path, util_update_path)
+
+util_source_path = f"./edited_lm_eval/task.py"
+util_update_path = f"{os.path.join(os.path.dirname(lm_eval.__file__), f'api/task.py')}"
+
+shutil.copyfile(util_source_path, util_update_path)
+
+# Huggingface Evaluate's Perplexity
 util_source_path = f"./perplexity.py"
 util_update_path = f"{os.path.join(os.path.dirname(evaluate.__file__), f'metrics/perplexity/perplexity.py')}"
 
